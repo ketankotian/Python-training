9e15ee7dfeced8681e9ed55ba84b25b19c43a74f
ketankotian <krkkotian@gmail.ocm>
2021-06-01 01:12:30 +0530

    python training assignments

--- day6.py
+++ day6.py
@@ -1,1 +1,21 @@
-# 1.
+#1.
+class Student:
+    student_name = 'Ketan'
+    marks = 92
+
+print(f"Student Name: {getattr(Student, 'student_name')}")
+print(f"Marks: {getattr(Student, 'marks')}")
+setattr(Student, 'student_name', 'Raghu')
+setattr(Student, 'marks', 93)
+print(f"Student Name: {getattr(Student, 'student_name')}")
+print(f"Marks: {getattr(Student, 'marks')}")
+
+#2.
+
+class Student:
+    student_id = '10'
+    student_name = 'ketan kotian'
+    def display():
+        print(f'Student id: {Student.student_id}\nStudent Name: {Student.student_name}')
+print("Original attributes and their values of the Student class:")
+Student.display()
